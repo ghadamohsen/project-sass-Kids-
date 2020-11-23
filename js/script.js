cffac77c2bd8de423fadb9d4d814213939726472
Ghada Eid Hassan <64067901+Ghada19@users.noreply.github.com>
2020-11-23 20:38:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const menu=document.querySelector('.menu-btn');
+menu.addEventListener('click' , ()=>{
+    menu.classList.toggle('change');
+    document.querySelector('.navbar').classList.toggle('change');
+});
+
+document.querySelectorAll('.card-btn').forEach(btn=>{
+    btn.addEventListener('click',()=>{
+        btn.parentElement.classList.toggle('change');
+    });
+});
